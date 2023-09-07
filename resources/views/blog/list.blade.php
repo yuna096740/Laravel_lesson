1bbe752c78f1a10e0ca11c6cf8e37e92fd92549b
yuna096740 <yyuusseeii1111@gmail.com>
2023-09-07 15:06:00 +0900

    [add] MVC, Bootstrap

---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts/layout')
+@section('title', 'ブログ一覧')
+@section('content')
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+    <h2>ブログ記事一覧</h2>
+    <table class="table table-striped">
+      <tr>
+        <th>記事番号</th>
+        <th>日付</th>
+        <th>タイトル</th>
+        <th></th>
+      </tr>
+
+      <tr>
+        <td>1</td>
+        <td>2023/09/07</td>
+        <td>テスト</td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <td>2</td>
+        <td>2023/09/07</td>
+        <td>テスト2</td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</div>
+@endsection
